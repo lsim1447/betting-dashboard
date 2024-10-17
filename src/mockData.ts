f647fe461b619f7845d71696783e4587d76fef58
Szilard Lazar <lazar.szilard1995@gmail.com>
2024-10-17 19:06:52 +0300

    fix: extend mocked data

--- src/mockData.ts
+++ src/mockData.ts
@@ -95,7 +95,7 @@ export const games = [
     teamA: {
       name: 'Los Angeles Dodgers',
       logoUrl:
-        'https://upload.wikimedia.org/wikipedia/en/6/63/Los_Angeles_Dodgers_logo.svg',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Los_Angeles_Dodgers_Logo.svg/640px-Los_Angeles_Dodgers_Logo.svg.png',
       odds: 1.8,
       betsPlaced: 175,
     },
@@ -113,7 +113,7 @@ export const games = [
     teamA: {
       name: 'Juventus',
       logoUrl:
-        'https://upload.wikimedia.org/wikipedia/en/3/33/Juventus_FC_2017_logo.svg',
+        'https://ssl.gstatic.com/onebox/media/sports/logos/6lal-0xwWtos5HI99HRvuQ_64x64.png',
       odds: 1.9,
       betsPlaced: 130,
     },
@@ -131,7 +131,7 @@ export const games = [
     teamA: {
       name: 'Brooklyn Nets',
       logoUrl:
-        'https://upload.wikimedia.org/wikipedia/en/4/44/Brooklyn_Nets_newlogo.svg',
+        'https://upload.wikimedia.org/wikipedia/en/thumb/4/40/Brooklyn_Nets_primary_icon_logo_2024.svg/1200px-Brooklyn_Nets_primary_icon_logo_2024.svg.png',
       odds: 2.0,
       betsPlaced: 105,
     },
@@ -149,7 +149,7 @@ export const games = [
     teamA: {
       name: 'Houston Astros',
       logoUrl:
-        'https://upload.wikimedia.org/wikipedia/en/0/02/Houston_Astros_logo.svg',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtyEbVVf1w0EqAlA_5ACPlNGDwH76yCIXd5g&s',
       odds: 2.1,
       betsPlaced: 120,
     },
@@ -159,6 +159,205 @@ export const games = [
         'https://upload.wikimedia.org/wikipedia/en/f/f2/Atlanta_Braves.svg',
       odds: 1.9,
       betsPlaced: 130,
+    },
+  },
+  {
+    id: 10,
+    sport: 'Soccer',
+    teamA: {
+      name: 'Chelsea',
+      logoUrl:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Chelsea_FC.svg/800px-Chelsea_FC.svg.png',
+      odds: 2.3,
+      betsPlaced: 100,
+    },
+    teamB: {
+      name: 'Manchester City',
+      logoUrl:
+        'https://ssl.gstatic.com/onebox/media/sports/logos/z44l-a0W1v5FmgPnemV6Xw_64x64.png',
+      odds: 1.6,
+      betsPlaced: 130,
+    },
+  },
+  {
+    id: 11,
+    sport: 'Basketball',
+    teamA: {
+      name: 'Boston Celtics',
+      logoUrl:
+        'https://upload.wikimedia.org/wikipedia/en/8/8f/Boston_Celtics.svg',
+      odds: 1.9,
+      betsPlaced: 115,
+    },
+    teamB: {
+      name: 'Toronto Raptors',
+      logoUrl:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/3/36/Toronto_Raptors_logo.svg/200px-Toronto_Raptors_logo.svg.png',
+      odds: 2.0,
+      betsPlaced: 140,
+    },
+  },
+  {
+    id: 12,
+    sport: 'Baseball',
+    teamA: {
+      name: 'Chicago Cubs',
+      logoUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1Nqt-i1x0NlWSTRc9wm7D1OAE07xjqgfizw&s',
+      odds: 1.7,
+      betsPlaced: 160,
+    },
+    teamB: {
+      name: 'St. Louis Cardinals',
+      logoUrl:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/9/9d/St._Louis_Cardinals_logo.svg/1200px-St._Louis_Cardinals_logo.svg.png',
+      odds: 2.2,
+      betsPlaced: 130,
+    },
+  },
+  {
+    id: 13,
+    sport: 'Soccer',
+    teamA: {
+      name: 'Atletico Madrid',
+      logoUrl:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/f/f9/Atletico_Madrid_Logo_2024.svg/800px-Atletico_Madrid_Logo_2024.svg.png',
+      odds: 1.8,
+      betsPlaced: 145,
+    },
+    teamB: {
+      name: 'Sevilla FC',
+      logoUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVzCarOTONpNZuTntLZwUu79_GlK5GGW5OAQ&s',
+      odds: 2.4,
+      betsPlaced: 90,
+    },
+  },
+  {
+    id: 14,
+    sport: 'Basketball',
+    teamA: {
+      name: 'Philadelphia 76ers',
+      logoUrl:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/0/0e/Philadelphia_76ers_logo.svg/1200px-Philadelphia_76ers_logo.svg.png',
+      odds: 2.2,
+      betsPlaced: 170,
+    },
+    teamB: {
+      name: 'Dallas Mavericks',
+      logoUrl:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/9/97/Dallas_Mavericks_logo.svg/800px-Dallas_Mavericks_logo.svg.png',
+      odds: 1.8,
+      betsPlaced: 130,
+    },
+  },
+
+  {
+    id: 16,
+    sport: 'Soccer',
+    teamA: {
+      name: 'Tottenham Hotspur',
+      logoUrl:
+        'https://upload.wikimedia.org/wikipedia/en/b/b4/Tottenham_Hotspur.svg',
+      odds: 2.0,
+      betsPlaced: 110,
+    },
+    teamB: {
+      name: 'Arsenal',
+      logoUrl: 'https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg',
+      odds: 1.9,
+      betsPlaced: 135,
+    },
+  },
+  {
+    id: 17,
+    sport: 'Basketball',
+    teamA: {
+      name: 'Miami Heat',
+      logoUrl:
+        'https://upload.wikimedia.org/wikipedia/en/f/fb/Miami_Heat_logo.svg',
+      odds: 2.5,
+      betsPlaced: 145,
+    },
+    teamB: {
+      name: 'San Antonio Spurs',
+      logoUrl:
+        'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nba/500/sa.png',
+      odds: 1.7,
+      betsPlaced: 100,
+    },
+  },
+  {
+    id: 18,
+    sport: 'Baseball',
+    teamA: {
+      name: 'Boston Red Sox',
+      logoUrl:
+        'https://upload.wikimedia.org/wikipedia/en/6/6d/RedSoxPrimary_HangingSocks.svg',
+      odds: 1.6,
+      betsPlaced: 155,
+    },
+    teamB: {
+      name: 'Tampa Bay Rays',
+      logoUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Tampa_Bay_Rays_Logo.svg/800px-Tampa_Bay_Rays_Logo.svg.png',
+      odds: 2.3,
+      betsPlaced: 115,
+    },
+  },
+  {
+    id: 19,
+    sport: 'Soccer',
+    teamA: {
+      name: 'Paris Saint-Germain',
+      logoUrl:
+        'https://ssl.gstatic.com/onebox/media/sports/logos/mcpMspef1hwHwi9qrfp4YQ_64x64.png',
+      odds: 1.4,
+      betsPlaced: 200,
+    },
+    teamB: {
+      name: 'Olympique Marseille',
+      logoUrl:
+        'https://ssl.gstatic.com/onebox/media/sports/logos/KfBX1kHNj26r9NxpqNaTkA_64x64.png',
+      odds: 3.0,
+      betsPlaced: 90,
+    },
+  },
+  {
+    id: 20,
+    sport: 'Basketball',
+    teamA: {
+      name: 'Denver Nuggets',
+      logoUrl:
+        'https://upload.wikimedia.org/wikipedia/en/7/76/Denver_Nuggets.svg',
+      odds: 1.8,
+      betsPlaced: 125,
+    },
+    teamB: {
+      name: 'Portland Trail Blazers',
+      logoUrl:
+        'https://upload.wikimedia.org/wikipedia/en/2/21/Portland_Trail_Blazers_logo.svg',
+      odds: 2.1,
+      betsPlaced: 140,
+    },
+  },
+
+  {
+    id: 22,
+    sport: 'Soccer',
+    teamA: {
+      name: 'Borussia Dortmund',
+      logoUrl:
+        'https://ssl.gstatic.com/onebox/media/sports/logos/FZnTSH2rbHFos4BnlWAItw_64x64.png',
+      odds: 1.7,
+      betsPlaced: 140,
+    },
+    teamB: {
+      name: 'Bayern Munich',
+      logoUrl:
+        'https://ssl.gstatic.com/onebox/media/sports/logos/-_cmntP5q_pHL7g5LfkRiw_64x64.png',
+      odds: 2.0,
+      betsPlaced: 160,
     },
   },
 ];
