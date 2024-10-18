d4987bf54ebfa2de8966a565c985c5d86be0adf9
Szilard Lazar <lazar.szilard1995@gmail.com>
2024-10-18 14:34:07 +0300

    feat: add transitions to cards

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -6,12 +6,17 @@ import { placeBet } from '../../store/features/gameSlice';
 import { GameCard } from '../GameCard';
 import { BetModal } from '../BetModal';
 import { Game } from '../../types';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   justify-items: center;
   gap: 50px;
+`;
+
+const MotionCard = styled(motion.div)`
+  width: 100%;
 `;
 
 export const Dashboard: React.FC = () => {
@@ -37,10 +42,20 @@ export const Dashboard: React.FC = () => {
         {filteredGames?.length === 0 && (
           <div>There are no matching sport events.</div>
         )}
-        {filteredGames.length > 0 &&
-          filteredGames.map((game) => (
-            <GameCard key={game.id} game={game} onBetClick={handleBetClick} />
-          ))}
+        <AnimatePresence>
+          {filteredGames.length > 0 &&
+            filteredGames.map((game) => (
+              <MotionCard
+                key={game.id}
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 50 }}
+                transition={{ duration: 0.4, ease: 'easeInOut' }}
+              >
+                <GameCard game={game} onBetClick={handleBetClick} />
+              </MotionCard>
+            ))}
+        </AnimatePresence>
       </Grid>
 
       {selectedGame && (
